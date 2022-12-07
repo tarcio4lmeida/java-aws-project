8d7f28e2f62c2690a67a537f32a998b65823f1a4
tarcio almeida <tarcioalmeidalima@gmail.com>
2022-12-07 16:53:41 -0300

    Criando Cluster, Vpc e service no cloudformation

--- aws-cdk/src/main/java/com/myorg/AwsCdkApp.java
+++ aws-cdk/src/main/java/com/myorg/AwsCdkApp.java
@@ -2,12 +2,17 @@ package com.myorg;
 
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class AwsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new VpcStack(app, "Vpc");
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
+        Vpc vpc = vpcStack.getVpc();
+
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpc);
+        clusterStack.addDependency(vpcStack);
 
         app.synth();
     }
--- aws-cdk/src/main/java/com/myorg/VpcStack.java
+++ aws-cdk/src/main/java/com/myorg/VpcStack.java
@@ -8,6 +8,9 @@ import software.constructs.Construct;
 // import software.amazon.awscdk.services.sqs.Queue;
 
 public class VpcStack extends Stack {
+
+    private Vpc vpc;
+
     public VpcStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -15,8 +18,13 @@ public class VpcStack extends Stack {
     public VpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Vpc.Builder.create(this, "Vpc01")
+        vpc = Vpc.Builder.create(this, "Vpc01")
                 .maxAzs(3)
+                .natGateways(0)
                 .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
     }
 }
